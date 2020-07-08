393dbf39df044c0b5d94d3a379845026accefd02
Carlos Schults <carlos@carlosschults.net>
2020-07-08 08:45:53 -0300

    Add first failing test
    I've added the first failing test to the StringCalculatorTest class, which
    doesn't compile, since the production class doesn't exist yet.

--- src/StringCalculatorKata.Test/StringCalculatorTest.cs
+++ src/StringCalculatorKata.Test/StringCalculatorTest.cs
@@ -4,5 +4,10 @@ namespace carlosschults.StringCalculatorKata.Test
 {
     public class StringCalculatorTest
     {
+        [Test]
+        public void Add_EmptyStringAsParam_ReturnsZero()
+        {
+            Assert.AreEqual(0, StringCalculator.Add(string.Empty));
+        }
     }
 }
