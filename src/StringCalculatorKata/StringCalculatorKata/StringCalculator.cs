1aadff5c42a6ed8258302b25240516f3c0b063b9
Carlos Schults <carlos@carlosschults.net>
2020-07-08 15:16:11 -0300

    Make test pass by throwing when negatives are specified

--- src/StringCalculatorKata/StringCalculatorKata/StringCalculator.cs
+++ src/StringCalculatorKata/StringCalculatorKata/StringCalculator.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace carlosschults.StringCalculatorKata
 {
@@ -8,12 +10,23 @@ namespace carlosschults.StringCalculatorKata
         {
             var parts = numbers.Split(',');
             var result = 0;
+            var negatives = new List<int>();
             
             foreach (var part in parts)
-            {
+            { 
                 int.TryParse(part, out int number);
-                if (number <= 1000)
+
+                if (number < 0)
+                    negatives.Add(number);
+                else if (number <= 1000)
                     result += number;
+            }
+
+            if (negatives.Count > 0)
+            {
+                var negativesList = string.Join(',', negatives);
+                var exceptionMessage = $"Negative numbers not allowed: {negativesList}.";
+                throw new ArgumentException(exceptionMessage);
             }
 
             return result;
