1dca19e2f87ef6184977ced74843e3608284581f
Carlos Schults <carlos@carlosschults.net>
2020-07-08 09:29:53 -0300

    Make the first test pass
    I've made the first test pass by creating the 'StringCalculator' class
    with the 'Add' method, returning zero.

---
+++ src/StringCalculatorKata/StringCalculatorKata/StringCalculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace carlosschults.StringCalculatorKata
+{
+    public class StringCalculator
+    {
+        public static int Add(string numbers)
+        {
+            return 0;
+        }
+    }
+}
